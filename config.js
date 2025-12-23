c8eb31444567ed74a804f90520f3fb31449d8780
farel <farelpratamaputra147@gmail.com>
2025-12-23 21:09:46 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// Configuration file template
+// Copy this file to config.js and fill with your actual values
+const CONFIG = {
+    TELEGRAM_BOT_TOKEN: '8161644789:AAGUDo2trAIuLL5MSDOWF6_3XEYXc1hvT8k',
+    TELEGRAM_CHAT_ID: '8494965854'
+};
